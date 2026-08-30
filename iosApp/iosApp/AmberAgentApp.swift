0f9848eb9e982109c5e774c33f746dd9d5c60a7f
Arquiel <arquiel@ArquieldeMini-2.lan>
2026-08-31 01:44:06 +0800

    fix(ios): stop language changes rebuilding app shell

--- iosApp/iosApp/AmberAgentApp.swift
+++ iosApp/iosApp/AmberAgentApp.swift
@@ -3,15 +3,9 @@ import SwiftUI
 @main
 struct AmberAgentApp: App {
     @State private var settingsStore = SettingsStore()
-    @AppStorage(IOSAppLanguagePreference.defaultsKey)
-    private var appLanguage = IOSAppLanguage.system.rawValue
 
     init() {
         IOSAppLanguagePreference.normalize()
-    }
-
-    private var selectedLanguage: IOSAppLanguage {
-        IOSAppLanguage(storedValue: appLanguage)
     }
 
     var body: some Scene {
@@ -23,7 +17,6 @@ struct AmberAgentApp: App {
                 AppShell(settingsStore: settingsStore)
 #endif
             }
-            .environment(\.locale, selectedLanguage.resolvedLocale())
         }
     }
 }
